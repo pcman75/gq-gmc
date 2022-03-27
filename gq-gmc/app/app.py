6f9abb94fa4804779d123acaf53b35db35663b59
Cosmin Manoliu <manoliu@gmail.com>
2022-03-27 10:40:03 +0300

    test

--- gq-gmc/app/app.py
+++ gq-gmc/app/app.py
@@ -35,8 +35,8 @@ def readCPM():
                 value = srec[0] << 8 | srec[1]
                 value = value & 0x3fff   # mask out high bits, as for CPS* calls on 300 series counters
                 logger.info(f"CPM = {value}")
-                triggerSensor("sensor.gmc_gq_cpm", "Nuclear Radiation CPM", value, logger)
-                triggerSensor("sensor.gmc_gq_usvh", "Nuclear Radiation μSvh", value * 0.39/60, logger)
+                triggerSensor("sensor.gmc_gq_cpm1", "Nuclear Radiation CPM 1", value, logger)
+                triggerSensor("sensor.gmc_gq_usvh1", "Nuclear Radiation μSvh 1", value * 0.39/60, logger)
         
     except Exception as e:
         logger.error(e)  
